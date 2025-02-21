ddaeb8154f07a83a64c1bf1afab7a8654fb0b45b
destinee-horvath <destinee.horvath@gmail.com>
2025-02-21 18:00:51 +1100

    implement latency test

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 # main script
 import threading
 from traffic_monitor import monitor_traffic, get_total_traffic, get_packet_counts
+from latency_test import latency_test
 
 '''
 Callback function to handle network traffic
@@ -8,17 +9,16 @@ Callback function to handle network traffic
     packet_data (dict)
 '''
 def handle_packet(packet_data):
-    print("Packet Captured:", packet_data)
+    # print("Packet Captured:", packet_data)
+    pass
 
 if __name__ == "__main__":
     #monitor in a separate thread
     threading.Thread(target=monitor_traffic, args=(handle_packet,), daemon=True).start()
     
-    #print packet traffic after some time 
+    
     while True:
-        # print("----- Total Packet Traffic:", get_total_traffic(), "-----")
-        # print("Packet Counts Per IP:", get_packet_counts())
-
+        # Show packet traffic
         print("\n------------------------------------------------------------------------------------------------------\n")
 
         print("----- Total Packet Traffic -----")
@@ -30,7 +30,20 @@ if __name__ == "__main__":
             for ip, counts in packet_counts.items():
                 print(f"IP: {ip} | Incoming: {counts['incoming']} | Outgoing: {counts['outgoing']}")
 
+        # Run latency test
+        print("\n----- Latency Per IP -----")
+        if packet_counts:
+            packet_counts_copy = packet_counts.copy() #make copy to avoid RuntimeError
+
+            for ip in packet_counts_copy.keys():
+                latency = latency_test(ip)
+                if latency is not None:
+                    print(f"IP: {ip} | Latency: {latency} ms")
+                else:
+                    print(f"IP: {ip} | Timed Out")
+
         print("\n------------------------------------------------------------------------------------------------------\n")
 
-        threading.Event().wait(5)
+
+        threading.Event().wait(5) #print packet traffic after some time 
 
